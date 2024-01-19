884c150818e8d543729e94616f744c8448d81adf
Ajay Thakur <127669499+ajaythakur3369@users.noreply.github.com>
2024-01-19 21:49:16 +0530

    Add files via upload

---
+++ 3_December_2023.py
@@ -0,0 +1,24 @@
+'''
+2. What will be the output of the following code?
+'''
+
+import collections 
+b = collections.Counter([2, 2, 3, 4, 4, 4])
+b.most_common(1)
+a = collections.Counter([2,2,3,3,3,4])
+b = collections.Counter([2,2,3,4,4])
+a|b
+
+'''
+Output:- Counter({2: 2, 3: 3, 4: 2})
+'''
+
+'''
+3. Which of the following commands is used to run java code? 
+Answer:- java file_Name 
+'''
+
+'''
+4. When is the constructor of a class or structure called in C++? 
+Answer:- An object is created
+'''

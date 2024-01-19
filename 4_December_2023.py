4b2bf3c4101186bd52876980ca8090e1ccb2c065
Ajay Thakur <127669499+ajaythakur3369@users.noreply.github.com>
2024-01-20 00:09:35 +0530

    Add files via upload

---
+++ 4_December_2023.py
@@ -0,0 +1,48 @@
+'''
+1. What will be the output of the following Python code?
+'''
+
+def foo():
+    try:
+        return 1
+    finally:
+        return 2
+
+# Driver’s code
+k = foo()
+print(k)
+
+'''
+Output:- 2
+'''
+
+'''
+2. What will be the output of the following statement?
+'''
+
+z = True and not (False)
+print(z)
+
+'''
+Output:- True
+'''
+
+'''
+3. Consider the schema R(S, T, U, V) and the depencies S->T, T->U, U->V, V->S. Let R = (R1, R2) such that R1⋂R2 = φ. Then the decomposition is - 
+Answer:- in both 2NF and 3NF
+'''
+
+'''
+4. Constructor is executed when - 
+Answer:- An object is created 
+'''
+
+
+
+
+
+
+
+
+
+

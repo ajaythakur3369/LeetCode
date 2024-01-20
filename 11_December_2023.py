2ebd88c03584fdf7f02b305624e08aa0e5f6502c
Ajay Thakur <127669499+ajaythakur3369@users.noreply.github.com>
2024-01-20 11:03:09 +0530

    Add files via upload

---
+++ 11_December_2023.py
@@ -0,0 +1,20 @@
+'''
+3. Data members and member functions of a class in the Python program are by default.
+Answer:- public
+'''
+
+'''
+4. What will be the output of the following statement?
+'''
+
+print(5 // 2) 
+
+'''
+Output:- 2
+'''
+
+'''
+5. Which of the following is not an advantage of encapsulation?
+Answer:- shorter code (are - Code reusability, Encapsulated code is better for unit testing, It protected data)
+'''
+

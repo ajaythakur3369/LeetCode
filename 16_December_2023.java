275182c21e8a9d3bd302d694b3a14b0475bfcda4
Ajay Thakur <127669499+ajaythakur3369@users.noreply.github.com>
2024-01-20 13:10:49 +0530

    Add files via upload

---
+++ 16_December_2023.java
@@ -0,0 +1,64 @@
+/******************************************************************************
+1. Which of the following is not a limitation of view? 
+Answer:- Index Created on View Used Often (are - ORDER BY Does Not Work, Cross Database Queries Not Allowed in Indexed View, Adding Column is Expensive by Joining Table Outside View)
+*******************************************************************************/
+
+/******************************************************************************
+2. What is the output of the following code?
+*******************************************************************************/
+
+public class CodingNinjas {
+    public static void main(String[] args) {
+        String str1 = "Coding";
+        int len = str1.length();
+        String str2 = "";
+
+        while (len > len / 2) {
+            len--;
+            str2 += str1.charAt(len);
+        }
+
+        str1 = str1.toUpperCase();
+        str2 = str2.toLowerCase();
+        String str = str1 + str2;
+
+        if (str.contains("NGgn")) {
+            str += "Ninjas";
+        }
+
+        str = str.toLowerCase();
+        System.out.println(str);
+    }
+}
+
+/******************************************************************************
+Output:- codinggnidocninjas
+*******************************************************************************/
+
+/******************************************************************************
+3. Syntex errors are also known as parsing errors.
+Answer:- True
+*******************************************************************************/
+
+/******************************************************************************
+4. Which of these classes is suitable to be used during multithreading operations?
+Answer:- StringBuffer
+*******************************************************************************/
+
+/******************************************************************************
+5. Here are some words translated from an artificial language. 
+morpirquat means birdhouse
+beelmorpir means bluebird
+beelclak means bluebell
+Which word could mean "houseguest"?
+Answer:- Quathunde
+*******************************************************************************/
+
+
+
+
+
+
+
+
+

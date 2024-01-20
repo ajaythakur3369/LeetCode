f4b622cf9a46b41b41cacb223b15746f309392b8
Ajay Thakur <127669499+ajaythakur3369@users.noreply.github.com>
2024-01-20 09:33:20 +0530

    Add files via upload

---
+++ 7_December_2023.py
@@ -0,0 +1,38 @@
+'''
+1. Which header file is required for the manipulation of math functions in C++? 
+Answer:- cmath
+'''
+
+'''
+2. 9th June of leap year was on Thursday. Then what was the day of the day of the week on 17 February? 
+Answer:- Wednesday 
+'''
+
+'''
+3. Which statement is used to catch all types of exceptions?
+Answer:- catch(…)
+'''
+
+'''
+4. What will be the output of the following Python code?
+'''
+
+print(5, 6, 7, sep='*', end=' ')
+
+'''
+Output:- 5*6*7 
+'''
+
+'''
+5. Which among the following commands on printing will give 6 as the output?
+'''
+
+A = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+'''
+Answer:- A[1][2]
+'''

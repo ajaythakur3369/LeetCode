32c224926f9db9e6c326bc3ad177d18be766ae30
Ajay Thakur <127669499+ajaythakur3369@users.noreply.github.com>
2024-01-20 09:16:27 +0530

    Add files via upload

---
+++ 5_December_2023.py
@@ -0,0 +1,30 @@
+'''
+1. The condition can be prevented by requiring that a process requests all of its required resources at once time and blocking the process until all requests can be granted simultaneously.
+Answer:- Hold and Wait 
+'''
+
+'''
+2. A recursive relationship is a relationship between an entity and ____. 
+Answer:- itself
+'''
+
+'''
+3. Which functional dependency type is/are not present in the following dependencies? Empno -> EName, Salary, Deptno, DName DeptNo -> DName EmpNo -> DName  
+Answer:- Partial functional dependency 
+'''
+
+'''
+4. What gives the developer the ability to destroy object x?
+Answer:- Only the garbage collection system can destroy an object.
+'''
+
+'''
+5. What will be the output of the following code?
+'''
+
+name = 'saif'
+print(type(name))
+
+'''
+Output:- str
+'''

774597e7ab365d3516342fc899b54b33018e7f05
Ajay Thakur <127669499+ajaythakur3369@users.noreply.github.com>
2024-01-20 11:52:56 +0530

    Add files via upload

---
+++ 14_December_2023.py
@@ -0,0 +1,50 @@
+'''
+1. What will be the output of the following code?
+'''
+
+s = "abcd"
+b = s + str(2)
+print(s)
+
+'''
+Output:- Error
+'''
+
+'''
+2. What is the output of the following for loop and range() function?
+'''
+
+for num in range(2, 6, 1):
+    print(num, end=",")
+
+'''
+Output:- 2,3,4,5,
+'''
+
+'''
+3. Choose the word which best expresses the meaning of the word EMBEZZLE 
+Answer:- Misappropriate
+'''
+
+'''
+4. Which operator takes three operands?
+Answer:- Ternary operator 
+'''
+
+'''
+5. Select public IP’s from the following?
+Answer:- 8.8.8.8, 191.16.5.9
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+

ac371b22fdd47ef2387f19e63d2c2826fdb1f651
Ajay Thakur <127669499+ajaythakur3369@users.noreply.github.com>
2024-01-20 14:53:34 +0530

    Add files via upload

---
+++ 19_December_2023.cpp
@@ -0,0 +1,38 @@
+/******************************************************************************
+1. Which of the following are the advantages of encapsulation? 
+Answer:- All of the above (Code reusability, Encapsulated code is better for unit testing, It protected data)
+*******************************************************************************/
+
+/******************************************************************************
+2. Eight people are sitting around a round table facing inwards. Alex is two seats to Sophie’s left. Adam is three seats to Alex’s right. Michelle is two seats to Alex’s left. John is six seats to Lucy’s right. Edward is six seats to John’s right. Michael is not sitting next to Alex. Who sits one seat to Edward’s left? 
+Answer:- Michael
+*******************************************************************************/
+
+/******************************************************************************
+3. Which of the following correctly declares an array?
+Answer:- int array[10];
+*******************************************************************************/
+
+/******************************************************************************
+4. Which of the following methods is used to run garbage collection? 
+Answer:- System.gc()
+*******************************************************************************/
+
+/******************************************************************************
+5. Which of the following is a global scope variable?
+*******************************************************************************/
+
+#include <iostream>
+using namespace std;
+
+int g;
+int local;
+
+int main() {
+    int global;
+    double b, c;
+}
+
+/******************************************************************************
+Answer:- local, g
+*******************************************************************************/

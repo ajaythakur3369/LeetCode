f690a847a1dff80962577fce2192e95d9c845828
Ajay Thakur <127669499+ajaythakur3369@users.noreply.github.com>
2024-01-20 12:08:45 +0530

    Add files via upload

---
+++ 15_December_2023.py
@@ -0,0 +1,37 @@
+'''
+1. Which of the following will run without errors?
+Answer:- round(45.8)
+'''
+
+'''
+2. What will be the output of the following statement?
+'''
+
+a = 5
+b = 5
+print(a is b)
+
+'''
+Output:- True 
+'''
+
+'''
+3. How is Shubham related to Shivani? Shubham is the brother of Meenal. Shivani is the niece of Pooja Neeraj Meenal’s uncle and Preeti’s brother.
+Answer:- The data given in both I and II together are not sufficient to answer the question.
+'''
+
+'''
+4. Which is the most optimal scheduling algorithm? (Persistent) 
+Answer:- SJF – Shortest Job First 
+'''
+
+'''
+5. What will be the output of the following statement?
+'''
+
+a = 2
+print(a << 1)
+
+'''
+Output:- 4
+'''

1530d8dc490b95551e982a42e4de95f22f72b8fd
Ajay Thakur <127669499+ajaythakur3369@users.noreply.github.com>
2024-01-20 11:38:02 +0530

    Add files via upload

---
+++ 13_December_2023.py
@@ -0,0 +1,20 @@
+'''
+3. What is the output of the following for loop and range() function? 
+'''
+
+for num in range(2, -5, -1):
+    print(num, end=",")
+
+'''
+Output:- 2,1,0,-1,-2,-3,-4,
+'''
+
+'''
+4. In the bounded buffer problem ___ 
+Answer:- there are n buffers ( n being greater than one but finite)
+'''
+
+'''
+5. Which of the following datatypes is used to store objects of different datatypes?
+Answer:- Class
+'''
